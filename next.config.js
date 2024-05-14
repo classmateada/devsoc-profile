f4ad28ac8d65078ca711a8d6f3993f0348637ebc
classmateada <classmateada@gmail.com>
2024-05-14 16:44:31 +0800

    Fix basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: 'devsoc-profile',
+  basePath: '/devsoc-profile',
   images: {
     unoptimized: true,
   },
