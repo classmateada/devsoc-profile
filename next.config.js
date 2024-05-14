deb3b18311cdd3d07cf31185b24dbd8f814af79a
classmateada <classmateada@gmail.com>
2024-05-14 16:40:25 +0800

    Add config for deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs'
+  basePath: 'devsoc-profile',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
