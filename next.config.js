db4252861052c11ae9d1ea7cbc807e4bab4cd4d5
classmateada <classmateada@gmail.com>
2024-05-14 16:31:21 +0800

    Remove basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
-  basePath: '/devsoc-profile'
+  distDir: 'docs'
 }
 
 module.exports = nextConfig
